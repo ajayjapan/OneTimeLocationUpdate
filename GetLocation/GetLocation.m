916525feb78d41e76a61efd5e247af3b63698278
Ajay Chainani <ajay.chainani@gmail.com>
2015-05-17 14:47:32 -0400

    request when in use authorization

--- GetLocation/GetLocation.m
+++ GetLocation/GetLocation.m
@@ -20,16 +20,16 @@ NSString * const ACGetLocationErrorDomain = @"ACGetLocationErrorDomain";
 @synthesize locationManager, timer, gotLocation, waitTime;
 
 + (GetLocation *)sharedInstance {
-    static GetLocation *_sharedInstance = nil;
-    static dispatch_once_t oncePredicate;
-    dispatch_once(&oncePredicate, ^{
-        _sharedInstance = [[self alloc] init];
-    });
-    
-    return _sharedInstance;
+  static GetLocation *_sharedInstance = nil;
+  static dispatch_once_t oncePredicate;
+  dispatch_once(&oncePredicate, ^{
+    _sharedInstance = [[self alloc] init];
+  });
+  
+  return _sharedInstance;
 }
 
-- (id)init{ 
+- (id)init{
   if (self = [super init]){
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
@@ -40,97 +40,92 @@ NSString * const ACGetLocationErrorDomain = @"ACGetLocationErrorDomain";
 }
 
 
--(void)pushBest
-{
-    CLLocation *location = self.locationManager.location;
+-(void)pushBest {
+  CLLocation *location = self.locationManager.location;
+  
+  if (!location) {
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObject:NSLocalizedStringFromTable(@"Couldn't get a location within the time limit", @"ACGetLocation", nil) forKey:NSLocalizedFailureReasonErrorKey];
+    NSError *error = [[NSError alloc] initWithDomain:ACGetLocationErrorDomain
+                                                code:kCLErrorLocationUnknown
+                                            userInfo:userInfo];
+    [self locationManager:self.locationManager didFailWithError:error];
+    return;
+  }
+  
+  if(!gotLocation) {
     
-    if (!location) {
-        NSDictionary *userInfo = [NSDictionary dictionaryWithObject:NSLocalizedStringFromTable(@"Couldn't get a location within the time limit", @"ACGetLocation", nil) forKey:NSLocalizedFailureReasonErrorKey];
-        NSError *error = [[NSError alloc] initWithDomain:ACGetLocationErrorDomain
-                                                    code:kCLErrorLocationUnknown
-                                                userInfo:userInfo];
-        [self locationManager:self.locationManager didFailWithError:error];
-        return;
-    }
+    gotLocation = YES;
     
-    if(!gotLocation) {
-        
-        gotLocation = YES;
-        
-        NSLog(@"stopUpdatingLocation");
-        [self.locationManager stopUpdatingLocation];
-        
-        NSLog(@"gotLocation");
-        [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationSuccessNotification
-                                                            object:location];
-        
-        NSLog(@"invalidate timer");
-        [timer invalidate];
-        self.timer = nil;
-    }
+    NSLog(@"stopUpdatingLocation");
+    [self.locationManager stopUpdatingLocation];
+    
+    NSLog(@"gotLocation");
+    [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationSuccessNotification
+                                                        object:location];
+    
+    NSLog(@"invalidate timer");
+    [timer invalidate];
+    self.timer = nil;
+  }
 }
 
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
-	
-	if(!gotLocation) {
-		NSLog(@"%f", newLocation.horizontalAccuracy);
-		if (newLocation.horizontalAccuracy <  DESIRED_ACCURACY && newLocation.horizontalAccuracy != -1) {
-			if ((newLocation.coordinate.longitude !=  0)&&(newLocation.coordinate.latitude !=  0)) {
-                if ((newLocation.coordinate.longitude !=  oldLocation.coordinate.longitude)&&(newLocation.coordinate.latitude !=  oldLocation.coordinate.latitude)) {
-                    gotLocation = YES;
-                    
-                    NSLog(@"gotLocation");
-                    [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationSuccessNotification
-                                                                        object:newLocation];
-
-                    NSLog(@"invalidate timer");
-                    [timer invalidate];
-                    self.timer = nil;
-                    
-                    NSLog(@"stopUpdatingLocation");
-                    [self.locationManager stopUpdatingLocation];
-                }
-			}
-		}
-	}
+  
+  if(!gotLocation) {
+    NSLog(@"%f", newLocation.horizontalAccuracy);
+    if (newLocation.horizontalAccuracy <  DESIRED_ACCURACY && newLocation.horizontalAccuracy != -1) {
+      if ((newLocation.coordinate.longitude !=  0)&&(newLocation.coordinate.latitude !=  0)) {
+        if ((newLocation.coordinate.longitude !=  oldLocation.coordinate.longitude)&&(newLocation.coordinate.latitude !=  oldLocation.coordinate.latitude)) {
+          gotLocation = YES;
+          
+          NSLog(@"gotLocation");
+          [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationSuccessNotification
+                                                              object:newLocation];
+          
+          NSLog(@"invalidate timer");
+          [timer invalidate];
+          self.timer = nil;
+          
+          NSLog(@"stopUpdatingLocation");
+          [self.locationManager stopUpdatingLocation];
+        }
+      }
+    }
+  }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-
-    [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationFailureNotification
-                                                        object:error];
-
+  [[NSNotificationCenter defaultCenter] postNotificationName:ACGetLocationFailureNotification
+                                                      object:error];
 }
 
 
 - (void)startTimer {
-  
-	
-	self.timer = [NSTimer scheduledTimerWithTimeInterval:self.waitTime
-                                                target:self 
-                                              selector:@selector(pushBest) 
-                                              userInfo:nil 
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:self.waitTime
+                                                target:self
+                                              selector:@selector(pushBest)
+                                              userInfo:nil
                                                repeats:NO];
-	
 }
 
 
 - (void)getCurrentLocation {
-	[self.locationManager startUpdatingLocation];
-	
-	[self startTimer];
-	
-	gotLocation = NO;
-	
-	NSLog(@"searching for location..");
+  if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+    [self.locationManager requestWhenInUseAuthorization];
+  }
 
+  [self.locationManager startUpdatingLocation];
+  
+  [self startTimer];
+  
+  gotLocation = NO;
+  
+  NSLog(@"searching for location..");
 }
 
 - (void)dealloc {
-  
-    self.locationManager.delegate = nil;
-
+  self.locationManager.delegate = nil;
 }
 
 @end
